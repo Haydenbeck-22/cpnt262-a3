8cd1a3f811489e8d8cc3f47c37895b434afe191f
Hayden Beck <haydenbeck22@gmail.com>
2021-11-01 00:05:46 -0600

    fixed function block

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 "use strict";
-  const useFetchedData = function(endpoint, dataHandler){
-    fetch(endpoint)
+  const useFetchedData = function(){
+    fetch('http://api.disneyapi.dev/characters/2500')
     .then(function(response){
       // JSON that is returned from the server must be converted to a JS object asynchronously.
       console.log(response);
@@ -9,9 +9,19 @@
       }
       return response.json();
     })
-    .then(function(data){
+    .then(function(character){
       // Any code that depends on the `data` must go in this block
-      dataHandler(data);
+      const container = document.querySelector('.container');
+      
+      container.innerHTML = `
+        <h1>${character.name}</h1>
+        <h2>Updated on ${character.updatedAt}</h2>
+        <h3>Featured in ${character.tvShows}</h3>
+        <img src=${character.imageUrl} alt="Captain Frost"</img>
+        <a href=${character.sourceUrl}>Wiki Page</a>
+      
+      `;
+      dataHandler(character);
     })
     .catch(function(err){
       // An error or `reject` from any of the above `.then()` blocks will end up here.
@@ -19,15 +29,18 @@
     });
   }
   
-  useFetchedData('http://api.disneyapi.dev/characters/2500', function(character){
-    const container = document.querySelector('.container');
+useFetchedData(function(character){
+  const container = document.querySelector('.container');
       
-    container.innerHTML = `
-      <h1>${character.name}</h1>
-      <h2>Updated on ${character.updatedAt}</h2>
-      <h3>Featured in ${character.tvShows}</h3>
-      <img src=${character.imageUrl} alt="Captain Frost"</img>
-      <a href=${character.sourceUrl}>Wiki Page</a>
-    
-    `;
-  });
+  container.innerHTML = `
+    <h1>${character.name}</h1>
+    <h2>Updated on ${character.updatedAt}</h2>
+    <h3>Featured in ${character.tvShows}</h3>
+    <img src=${character.imageUrl} alt="Captain Frost"</img>
+    <a href=${character.sourceUrl}>Wiki Page</a>
+  
+  `;
+
+
+
+})
